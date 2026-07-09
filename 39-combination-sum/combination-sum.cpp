2fa9b5a9fae9862d3bb8641202141d9d16c4ef2c
ARVIND RAWAT <arvind244715rwt@gmail.com>
2026-07-09 10:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (93.46%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
-    void intcombination(int ind,
+    void combination(int ind,
                         int target,
                         vector<int>& candidates,
                         vector<vector<int>>& result,
                         vector<int>& ans)
     {
-        int n = candidates.size();
-
-        if(ind == n)
+       int n = candidates.size();
+       if(ind == n)
+       {
+        if(target == 0)
         {
-            if(target == 0)
-            {
-                result.push_back(ans);
-            }
-            return;
+            result.push_back(ans);
         }
-
-        if(candidates[ind] <= target)
-        {
-            ans.push_back(candidates[ind]);
-
-            intcombination(ind,
-                           target - candidates[ind],
-                           candidates,
-                           result,
-                           ans);
-
-            ans.pop_back();
-        }
-
-        intcombination(ind + 1,
-                       target,
-                       candidates,
-                       result,
-                       ans);
+        return;
+       }
+       if(candidates[ind]<=target)
+       {
+        ans.push_back(candidates[ind]);
+        combination(ind,target-candidates[ind],candidates,result,ans);
+        ans.pop_back();
+       }
+       combination(ind+1,target,candidates,result,ans);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target)
@@ -42,7 +29,7 @@ public:
         vector<vector<int>> result;
         vector<int> ans;
 
-        intcombination(0, target, candidates, result, ans);
+        combination(0, target, candidates, result, ans);
 
         return result;
     }
