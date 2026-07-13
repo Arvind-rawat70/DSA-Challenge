71e4a00ee5f2e247c2ea6fa981bf482e30fb5cf2
ARVIND RAWAT <arvind244715rwt@gmail.com>
2026-07-13 14:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 87.3 MB (45.79%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) 
+    {
+        int five = 0;
+        int ten =0;
+        for(int i = 0; i<bills.size();i++)
+        {
+            if(bills[0]!=5)
+            {
+                return false;
+            }
+            if(bills[i]==5)
+            {
+                five ++;
+            }
+            else if(bills[i]==10 && five>0)
+            {
+               ten++;
+               five--;
+            }
+            else 
+            {   
+                if (ten > 0 && five > 0)
+                {
+                    ten--;
+                    five--;
+                }
+             
+                else if (five >= 3)
+                {
+                    five -= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
