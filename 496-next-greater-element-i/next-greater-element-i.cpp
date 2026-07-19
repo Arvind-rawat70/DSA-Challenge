9ecdb97d66439105d1ee7bf083d6c65a665677ab
ARVIND RAWAT <arvind244715rwt@gmail.com>
2026-07-19 09:08:14 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (15.77%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
-        vector<int>result;
-        int k = nums2.size();
-        int j = 0;
-        for(int i = 0; i<nums1.size();i++)
+     stack<int>st;
+     unordered_map<int,int>map;
+     int n = nums1.size();
+     vector<int>nse(n);
+     for(int i = nums2.size()-1; i>=0; i--)
+     {
+        while(!st.empty() && nums2[i]>=st.top())
         {
-            int ans = -1;
-            for(int j = 0; j<nums2.size();j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    for(int k = j+1; k<nums2.size(); k++)
-                    {
-                        if(nums2[k]>nums2[j])
-                        {
-                            ans = nums2[k];
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-            result.push_back(ans);
+            st.pop();
         }
-        return result;        
+        if(st.empty())
+        {
+            map[nums2[i]] = -1;
+        }else{
+            map[nums2[i]] = st.top();
+        }
+        st.push(nums2[i]);
+     }
+     for(int i = 0; i<nums1.size();i++)
+     {
+        nse[i] = map[nums1[i]];
+     }
+     return nse;
     }
 };
