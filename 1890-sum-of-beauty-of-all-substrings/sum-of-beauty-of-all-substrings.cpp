e30236c5786c6b1dabbdc102fd7a8c49019e79ec
ARVIND RAWAT <arvind244715rwt@gmail.com>
2026-09-12 18:23:53 +0530

    Time: 116 ms (47.71%) | Memory: 8.9 MB (89.96%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -3,27 +3,25 @@ public:
     int beautySum(string s) 
     {
         int sum = 0;
-        for(int i = 0; i<s.size(); i++)
+        for(int i = 0; i<s.length(); i++)
         {
-            vector<int>freq(26,0);
-            for(int j = i; j<s.size(); j++)
+            int freq[26] = {0};
+            for(int j = i; j<s.length(); j++)
             {
                 freq[s[j]-'a']++;
                 int mx = 0;
                 int mn = INT_MAX;
-                for(int k=0; k<26; k++)
+                for(int k =0; k<26; k++)
                 {
                     if(freq[k]>0)
                     {
-                        mx = max(mx,freq[k]);
-                        mn = min(mn,freq[k]);
+                        mx = max(freq[k],mx);
+                        mn = min(freq[k],mn);
                     }
                 }
-                sum+=(mx-mn);
-
+                sum +=(mx-mn);
             }
         }
         return sum;
-        
     }
 };
