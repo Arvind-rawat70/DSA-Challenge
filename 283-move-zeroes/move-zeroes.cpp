c8225b27648e503ca62871b1cf2ba9222aa8d0f8
ARVIND RAWAT <arvind244715rwt@gmail.com>
2026-09-17 10:18:31 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.50%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) 
     {
-        int index = -1;
-
-        // Find first zero
-        for (int i = 0; i < nums.size(); i++)
+        int i = 0;
+        int j = i+1;
+        while(j<nums.size())
         {
-            if (nums[i] == 0)
+            if(nums[i]==0 && nums[j]!=0)
             {
-                index = i;
-                break;
-            }
-        }
-        if(index==-1)
-        {
-            return ;
-        }
-
-        int i = index;
-        int j = index + 1;
-
-        while (j < nums.size())
-        {
-            if (nums[j] != 0)
-            {
-                swap(nums[i], nums[j]);
+                swap(nums[i],nums[j]);
                 i++;
+                j++;
             }
-
-            j++;
+            else if(nums[i]==0 && nums[j]==0)
+            {
+                j++;
+            }
+            else 
+            {
+                i++;
+                j++;
+            }
         }
     }
 };
