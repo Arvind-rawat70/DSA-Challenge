4bc96df43de50bd357893e58c38cab9e1998c43b
ARVIND RAWAT <arvind244715rwt@gmail.com>
2026-09-18 11:56:55 +0530

    Time: 7 ms (40.33%) | Memory: 26.6 MB (21.50%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,19 +3,19 @@ public:
     vector<int> majorityElement(vector<int>& nums) 
     {
         vector<int>result;
-        unordered_map<int,int>map;
-        for(int i = 0; i<nums.size(); i++)
+        unordered_map<int, int>map;
+        for(int i = 0;i<nums.size(); i++)
         {
             map[nums[i]]++;
         }
-        int n = nums.size()/3;
-        for(auto it :map)
+        int n = nums.size();
+        for(auto it: map)
         {
-            if(it.second>n)
+            if(it.second>n/3)
             {
                 result.push_back(it.first);
             }
         }
         return result;
-     }
+    }
 };
