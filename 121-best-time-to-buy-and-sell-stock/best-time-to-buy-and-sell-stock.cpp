4fbbb52f25fca21be3f04965f7bfcc5448523c77
ARVIND RAWAT <arvind244715rwt@gmail.com>
2026-09-19 22:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (26.29%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,14 +3,14 @@ public:
     int maxProfit(vector<int>& prices) 
     {
         int min_val = INT_MAX;
-        int max_val = 0;
-        for(int i = 0; i<prices.size(); i++)
+        int profit = 0;
+        for(int i= 0; i<prices.size(); i++)
         {
             min_val = min(min_val,prices[i]);
             int val = prices[i]-min_val;
-            max_val = max(max_val,val);
+            profit = max(profit,val);
         }
-        return max_val;
+        return profit;
         
     }
 };
